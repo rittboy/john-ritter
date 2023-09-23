1c36bf97126002ba93420d1adc238a507d3ad17f
John Ritter <john.ritter.8788@gmail.com>
2023-09-23 19:23:54 -0400

    working out kinks with project 9

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,13 +1,61 @@
-const body = document.querySelector('body');
-const modeToggle = document.getElementById('mode-toggle');
-const modeStatus = document.querySelector('.mode-status');
+// const body = document.querySelector('body');
+// const modeToggle = document.getElementById('mode-toggle');
+// const modeStatus = document.querySelector('.mode-status');
 
-function toggleMode(){
-  body.classList.toggle('dark-mode');
+// function toggleMode(){
+//   body.classList.toggle('dark-mode');
 
-  const modeMessage = body.classList.contains('dark-mode') ? 'Dark Mode' : "Light Mode"
+//   const modeMessage = body.classList.contains('dark-mode') ? 'Dark Mode' : "Light Mode"
 
-  modeStatus.innerText = "Currently in " + modeMessage;
+//   modeStatus.innerText = "Currently in " + modeMessage;
+// }
+
+// modeToggle.addEventListener('click', toggleMode);
+
+const images = document.querySelectorAll('#slider img');
+const prevImage = document.getElementById('prev');
+const nextImage = document.getElementById('next');
+
+let curIndex = 0;
+
+function reset(){
+    for(let i = 0; i < images.length; i++){
+        images[i].classList.remove('active');
+    }
 }
 
-modeToggle.addEventListener('click', toggleMode);
+function initSlider(){
+    reset();
+    images[curIndex].classList.add('active');
+}
+
+function prevSlide(){
+    reset();
+    currentIndex--;
+    if(curIndex < 0){
+        curIndex = images.length - 1;
+    }
+
+    images[curIndex].classList.add('active');
+}
+
+function nextSlide(){
+    reset();
+    curIndex++;
+    if(curIndex >= images.length){
+        curIndex = 0;
+    }
+
+    images[curIndex].classList.add('active');
+}
+
+initSlider();
+
+prevImage.addEventListener('click', function(){
+    prevSlide();
+});
+
+nextImage.addEventListener('click', function(){
+    nextSlide();
+})
+
